b3479c19b72042aa813cf859e6f3c269e118b3b7
panda <SmelayaPanda@mail.ru>
2018-02-13 21:51:44 +0700

    load products from database

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var Product = require('../models/product');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    Product.find(function (err, docs) {
+        res.render('shop/index',
+            {
+                title: 'Express',
+                products: docs
+            });
+    });
 });
 
 module.exports = router;
