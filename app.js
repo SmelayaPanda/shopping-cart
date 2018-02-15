20ed801c6ae6c78709e81dfb200cb1a47cad9fa8
panda <SmelayaPanda@mail.ru>
2018-02-15 13:51:41 +0700

    shopping cart view

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(session(
         secret: 'mySuperPuperSecret',
         resave: false,
         saveUninitialized: false,
-        cookie: {maxAge: 60000},
+        cookie: {maxAge: 14 * 24 * 60 * 60 * 1000 },
         store: new MongoStore({ mongooseConnection: mongoose.connection })
         // store time to live - ttl: 14 * 24 * 60 * 60 // = 14 days. Default
     }));
--- routes/index.js
+++ routes/index.js
@@ -29,9 +29,18 @@ router.get('/add-to-cart/:id', function (req, res, next) {
         }
         cart.add(product, product.id);
         req.session.cart = cart;
-        console.log(req.session.cart);
         res.redirect('/');
     });
+});
+
+router.get('/shopping-cart/', function (req, res, next) {
+    console.log(req.session);
+    if (!req.session.cart) {
+        res.render('shop/shopping-cart', {products: null})
+    }
+    var cart = new Cart(req.session.cart);
+    res.render('shop/shopping-cart',
+        {products: cart.generateArray(), totalPrice: cart.totalPrice})
 });
 
 module.exports = router;
