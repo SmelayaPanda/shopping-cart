7f8be7d427a7929bf00d106175156428845af58a
panda <SmelayaPanda@mail.ru>
2018-02-17 13:55:49 +0700

    paypal

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,81 @@ var express = require('express')
     , stripe = require("stripe")("sk_test_q4FzqlpzhmmVlEN6Wq0aYQAI");
 ;
 
+const paypal = require('paypal-rest-sdk');
+paypal.configure({
+    'mode': 'sandbox', //sandbox or live
+    'client_id': 'Ad40922VN8SQtxW7n9nHrj1C7IGvm0ZTp7w60jqz7R8Xz9bRgUXC7J4RbkhRFNuifEh4jKyylbyrseEw',
+    'client_secret': 'EBjZF3A3Xu8MzeLxrcZp1EiQAJoUEf4gZhIb6aK8mkPMY9iX1QI0panPKgz3YoDCWjKamHbumZBF7aX6'
+});
+
+router.post('/pay', function (req, res) {
+    var create_payment_json = {
+        "intent": "sale",
+        "payer": {
+            "payment_method": "paypal"
+        },
+        "redirect_urls": {
+            "return_url": "http://localhost:3000/success",
+            "cancel_url": "http://localhost:3000/cancel"
+        },
+        "transactions": [{
+            "item_list": {
+                "items": [{
+                    "name": "Red Sox Hat",
+                    "sku": "001",
+                    "price": "25.00",
+                    "currency": "USD",
+                    "quantity": 1
+                }]
+            },
+            "amount": {
+                "currency": "USD",
+                "total": "25.00"
+            },
+            "description": "Hat for the best team ever"
+        }]
+    };
+
+    paypal.payment.create(create_payment_json, function (error, payment) {
+        if (error) {
+            throw error;
+        } else {
+            for (let i = 0; i < payment.links.length; i++) {
+                if (payment.links[i].rel === 'approval_url') {
+                    res.redirect(payment.links[i].href);
+                }
+            }
+        }
+    });
+
+});
+
+router.get('/success', function (req, res) {
+    const payerId = req.query.PayerID;
+    const paymentId = req.query.paymentId;
+
+    const execute_payment_json = {
+        "payer_id": payerId,
+        "transactions": [{
+            "amount": {
+                "currency": "USD",
+                "total": "25.00"
+            }
+        }]
+    };
+
+    paypal.payment.execute(paymentId, execute_payment_json, function (error, payment) {
+        if (error) {
+            console.log(error.response);
+            throw error;
+        } else {
+            console.log(JSON.stringify(payment));
+            res.send('Success');
+        }
+    });
+});
+
+
 var Product = require('../models/product');
 
 router.post('/charge', function (req, res, next) {
